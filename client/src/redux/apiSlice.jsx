8d40028d02716dec68ae61156b79f7acfa7a19c7
mayurkanade44 <mayurkanade44@gmail.com>
2023-09-09 15:43:28 +0530

    prod fix

--- client/src/redux/apiSlice.jsx
+++ client/src/redux/apiSlice.jsx
@@ -1,9 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { toast } from "react-toastify";
+import { logout } from "./authSlice";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+const baseQuery = fetchBaseQuery({ baseUrl: "/" });
+const authBaseQuery = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+    toast.error("Unauthorized!! logged out");
+    api.dispatch(logout());
+  }
+  return result;
+};
 
 export const apiSlice = createApi({
-  baseQuery,
+  baseQuery: authBaseQuery,
   tagTypes: ["User", "Blog", "Admin"],
   endpoints: (builder) => ({}),
 });
